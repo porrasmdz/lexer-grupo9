45e9e7d8cef5e1de686d2ddaea280c675a97709d
porrasmdz <aporrasmdz@gmail.com>
2024-06-17 21:39:35 -0500

    instanced logger

---
+++ app/logger_instance.py
@@ -0,0 +1,21 @@
+import logging
+from datetime import datetime
+
+def get_group_logger(analysis:str, name: str, timestamp:datetime):
+
+    logger = logging.getLogger(__name__)
+
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler(f'logs/{analysis}-{name}-{timestamp.date()}-{timestamp.time().hour}h{timestamp.time().minute}.txt')
+    c_handler.setLevel(logging.INFO)
+    f_handler.setLevel(logging.INFO)
+
+    c_format = logging.Formatter('%(asctime)s | %(message)s')
+    f_format = logging.Formatter('%(asctime)s | %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+    return logger
--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
-import logging
-from .ply.lex import lex
 
-logger = logging.getLogger(__name__)
+from logger_instance import get_group_logger
+from ply.lex import lex
+from datetime import datetime
 
+#Reemplazar por mi_nombre por tu nombre
+logger = get_group_logger("lexico", "mi_nombre", datetime.now())
+
+if __name__ == "__main__":
+    #Antes y despues de ejecutar una funcion usar esta funcion para ejecutar los logs
+    logger.warning("Min nombre")
